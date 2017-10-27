d5ce3d380a30dda19626ca8b45fedf198f27bf5c
Michael F. Herbst <info@michael-herbst.com>
2017-10-27 16:58:20 +0200

    Add RMSOl vs l plot for the ions

--- 10_results/RMSO_vs_l/plot.py
+++ 10_results/RMSO_vs_l/plot.py
@@ -10,28 +10,38 @@ import rmsl
 
 
 dir_of_this_file = os.path.dirname(__file__)
-nlm = (6, 5, 5)
+
+
+def datafile(atom):
+    n, l, m = (6, 5, 5)
+    file_655 = dir_of_this_file + "/" + atom + "_n" + str(n) + \
+        "-l" + str(l) + "-m" + str(m) + ".hdf5"
+    file_766 = dir_of_this_file + "/" + atom + "_n" + str(n + 1) + \
+        "-l" + str(l + 1) + "-m" + str(m + 1) + ".hdf5"
+    if os.path.isfile(file_655):
+        return file_655
+    else:
+        return file_766
 
 
 def plot_rmso_l(systems):
-    n, l, m = nlm
     plt.close()
     plt.figure(figsize=(5.5, 3.5))
 
     data = {}
     for atom in systems:
-        state = molsturm.load_hdf5(dir_of_this_file + "/" + atom +
-                                   "_n" + str(n) + "-l" + str(l) +
-                                   "-m" + str(l) + ".hdf5")
+        state = molsturm.load_hdf5(datafile(atom))
         lpop = rmsl.compute_rmso_q(state)
         ls = np.arange(0, len(lpop))
-        plt.semilogy(ls, lpop, "x-", label=atom)
+        plt.semilogy(ls, lpop, "x-", label=r"\ce{" + atom + "}")
         data[atom] = lpop.tolist()
 
     if systems[0] == "Li":
         name = "rmso_period2_data.yaml"
     elif systems[0] == "Na":
         name = "rmso_period3_data.yaml"
+    elif systems[0] == "B+":
+        name = "rmso_ions_data.yaml"
     else:
         raise NotImplementedError("Only first and second period implemented.")
     with open(name, "w") as f:
@@ -43,67 +53,11 @@ def plot_rmso_l(systems):
     plt.legend()
 
 
-def plot_rmsw_l(systems):
-    n, l, m = nlm
-    plt.close()
-
-    data = {}
-    for atom in systems:
-        state = molsturm.load_hdf5(dir_of_this_file + "/" + atom +
-                                   "_n" + str(n) + "-l" + str(l) +
-                                   "-m" + str(l) + ".hdf5")
-
-        lpop = rmsl.compute_rmsw_q(state)
-        ls = np.arange(0, len(lpop))
-        plt.semilogy(ls, lpop, "x-", label=atom)
-        data[atom] = lpop.tolist()
-
-    plt.ylim([1e-16, 1])
-    plt.xlabel(r"Orbital angular momentum value $l$")
-    plt.ylabel(r"$\text{RMSW}_l$")
-    plt.legend()
-
-
-def plot_rmsr_l(systems):
-    n, l, m = nlm
-    plt.close()
-
-    data = {}
-    for atom in systems:
-        state = molsturm.load_hdf5(dir_of_this_file + "/" + atom +
-                                   "_n" + str(n) + "-l" + str(l) +
-                                   "-m" + str(l) + ".hdf5")
-
-        factor = 5
-        lpop = rmsl.compute_rmsr_q(state, factor=factor)
-        ls = np.arange(0, len(lpop))
-        plt.semilogy(ls, lpop, "x-", label=atom)
-        data[atom] = lpop.tolist()
-
-    if systems[0] == "Li":
-        name = "rmsr_period2_data.yaml"
-    elif systems[0] == "Na":
-        name = "rmsr_period3_data.yaml"
-    else:
-        raise NotImplementedError("Only first and second period implemented.")
-    with open(name, "w") as f:
-        yaml.safe_dump(data, f)
-
-    plt.ylim([1e-16, 1])
-    plt.xlabel(r"Orbital angular momentum value $l$")
-    plt.ylabel(r"root mean square reachable orbital coefficients " +
-               str(factor) + " gap")
-    plt.legend()
-
-
 def plot_rms_lf(atom, orbitals=range(10), labels=None):
-    n, l, m = nlm
     plt.close()
     plt.figure(figsize=(5.5, 3.5))
 
-    state = molsturm.load_hdf5(dir_of_this_file + "/" + atom +
-                               "_n" + str(n) + "-l" + str(l) +
-                               "-m" + str(l) + ".hdf5")
+    state = molsturm.load_hdf5(datafile(atom))
     data = rmsl.compute_rms_qf(state)
     ls = np.arange(0, data[:, 1].size)
 
@@ -124,6 +78,7 @@ def setup():
         r"\usepackage[T1]{fontenc}",
         r"\usepackage{lmodern}",
         r"\usepackage{amsmath}",
+        r"\usepackage[version=3]{mhchem}",
     ]
     pgf_with_rc_fonts = {
         "pgf.texsystem": "pdflatex",
@@ -144,20 +99,9 @@ def main():
     plot_rmso_l(["Na", "Mg", "Al", "Si", "P", "S", "Cl", "Ar"])
     plt.savefig("rmso_period3_vs_l.pdf", bbox_inches="tight")
 
-    # TODO Not sure how to display the importance for correlation
-    #      methods.
-    # plot_rmsw_l(["Li", "Be", "B", "C", "N", "O", "F", "Ne"])
-    # plt.savefig("rmsw_period2_vs_l.pdf", bbox_inches="tight")
-    #
-    # plot_rmsw_l(["Na", "Mg", "Al", "Si", "P", "S", "Cl", "Ar"])
-    # plt.savefig("rmsw_period3_vs_l.pdf", bbox_inches="tight")
-    #
-    #
-    # plot_rmsr_l(["Li", "Be", "B", "C", "N", "O", "F", "Ne"])
-    # plt.savefig("rmsr_period2_vs_l.pdf", bbox_inches="tight")
-    #
-    # plot_rmsr_l(["Na", "Mg", "Al", "Si", "P", "S", "Cl", "Ar"])
-    # plt.savefig("rmsr_period3_vs_l.pdf", bbox_inches="tight")
+    # missing: Si-, "S+",
+    plot_rmso_l(["B+", "C-", "O+", "F-", "Al+", "Cl-"])
+    plt.savefig("rmso_ions_vs_l.pdf", bbox_inches="tight")
 
     plot_rms_lf("O")
     plt.savefig("rms_lf_O.pdf")
