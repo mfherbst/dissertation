5c38e54f39aa3a802487b8b5d2f44a4b41f89959
Michael F. Herbst <info@michael-herbst.com>
2018-01-25 16:17:27 +0100

    Add local energy and relative error plots

---
+++ 4_solving_hf/hydrogen_comparison/cgtos.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python3
+
+import molsturm
+import common
+from matplotlib import pyplot as plt
+import numpy as np
+
+
+BASIS_SETS = ["STO-3G", "cc-pVDZ", "pc-1", "pc-3", "cc-pV6Z"]
+
+
+def plot_overview():
+    plt.close()
+    plt.figure(figsize=(5.5, 3.5))
+
+    for baset in BASIS_SETS:
+        bas = molsturm.construct_basis("gaussian", "H", basis_set_name=baset)
+        coeff = common.do_hydrogen_scf(bas)
+
+        rab = np.linspace(0, 1, 5000), np.linspace(1, 10, 5000)
+        r = np.concatenate(rab)
+        r, locen = common.hydrogen_local_energy(bas, coeff, r, dr=1e-6)
+        plt.plot(r, locen, label=baset)
+
+    plt.xlabel(r"radial distance $r$ in Bohr")
+    plt.ylabel(r"$E_L(r)$ in Hartree")
+
+    plt.plot(r, -0.5*np.ones_like(locen), label="exact")
+    plt.legend(loc='upper right')
+    plt.ylim(-6, 6)
+
+
+def plot_closeup():
+    plt.close()
+    plt.figure(figsize=(5.5, 3.5))
+
+    for baset in BASIS_SETS:
+        bas = molsturm.construct_basis("gaussian", "H", basis_set_name=baset)
+        coeff = common.do_hydrogen_scf(bas)
+
+        rab = np.linspace(0, 0.05, 5000), np.linspace(0.05, 0.5, 5000)
+        r = np.concatenate(rab)
+        r, locen = common.hydrogen_local_energy(bas, coeff, r, dr=1e-6)
+        plt.plot(r, locen, label=baset)
+
+    plt.xlabel(r"radial distance $r$ in Bohr")
+    plt.ylabel(r"$E_L(r)$ in Hartree")
+
+    plt.plot(r, -0.5*np.ones_like(locen), label="exact")
+    plt.legend(loc='lower right')
+    plt.ylim(-6, 6)
+
+
+def plot_relative_error(log=False):
+    plt.close()
+    plt.figure(figsize=(5.5, 3.5))
+
+    for baset in BASIS_SETS:
+        bas = molsturm.construct_basis("gaussian", "H", basis_set_name=baset)
+        coeff = common.do_hydrogen_scf(bas)
+
+        rab = np.linspace(0, 1, 10000), np.linspace(1, 10, 10000)
+        r = np.concatenate(rab)
+        r, err = common.hydrogen_relative_error(bas, coeff, r)
+
+        if log:
+            plt.semilogy(r, np.abs(err), label=baset)
+            extra = "absolute of "
+        else:
+            plt.plot(r, err, label=baset)
+            extra = ""
+
+    plt.xlabel(r"radial distance $r$ in Bohr")
+    plt.ylabel(extra + r"relative error")
+
+    plt.legend(loc='lower right')
+    plt.ylim(-0.2, 0.2)
+
+
+def main():
+    common.setup()
+
+    plot_overview()
+    plt.savefig("local_energy_cgto.pdf", bbox_inches="tight")
+
+    plot_closeup()
+    plt.savefig("local_energy_cgto_zoom.pdf", bbox_inches="tight")
+
+    plot_relative_error(log=False)
+    plt.savefig("relative_error_cgto.pdf", bbox_inches="tight")
+
+
+if __name__ == "__main__":
+    main()
