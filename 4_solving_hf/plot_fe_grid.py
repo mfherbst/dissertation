57e6d25f149ea9eea07561f208eb1fef77d62603
Michael F. Herbst <info@michael-herbst.com>
2018-01-31 23:00:16 +0100

    FE section finished

--- 4_solving_hf/plot_fe_grid.py
+++ 4_solving_hf/plot_fe_grid.py
@@ -27,16 +27,48 @@ def setup():
 def get_cells():
     # labels at cell boundaries
     # position of grid cell boundary
-    return {
+    cells = {
         0: r"$a = x_0$",
-        10: r"",
-        17: r"$x_{j-1}$",
-        28: r"$x_j$",
-        45: r"$x_{j+1}$",
-        63: r"$x_{j+2}$",
-        89: r"",
-        100: r"$x_{\text{N}_\text{cell}} = b$",
+        10: r"$x_1$",
+        21: r"$x_{j-1}$",
+        29: r"$x_j$",
+        52: r"$x_{j+1}$",
+        69: r"$x_{j+2}$",
+        85: r"$x_{N_\text{cell}-1}$",
+        100: r"$x_{N_\text{cell}} = b$",
     }
+    x = np.linspace(min(cells.keys()), max(cells.keys()), 100*100)
+    return x, cells
+
+
+def make_cell_label(cells, i):
+    lals = ["", "$c_0$", "$\cdots$", "$c_{j-1}$", "$c_j$", "$c_{j+1}$",
+            "$\cdots$", r"$c_{N_\text{cell}}$"]
+    return lals[i]
+
+
+def plot_grid(cells):
+    keys = list(cells.keys())
+    for i, k in enumerate(keys):
+        if i > 0:
+            half = keys[i-1] + (keys[i] - keys[i-1]) / 2
+            plt.text(half, 0.9, make_cell_label(cells, i),
+                     ha="center", va="center",
+                     color="DarkGrey", fontsize=8)
+        plt.axvline(x=k, color="DarkGray", linewidth=0.4, linestyle="--")
+
+
+def plot_ticks(ax, cells, subticks=0):
+    keys = list(cells.keys())
+    keys_minor = []
+
+    for i, k in enumerate(keys):
+        if i > 0:
+            extra = np.linspace(keys[i-1], keys[i], subticks + 1, endpoint=False)
+            keys_minor += extra[1:].tolist()
+    plt.xticks(keys, list(cells.values()))
+    ax.set_xticks(keys_minor, minor=True)
+    ax.set_xticklabels(len(keys_minor) * [""], minor=True)
 
 
 def linear_element(cells, n, x):
@@ -52,21 +84,6 @@ def linear_element(cells, n, x):
     ret[lpart] = (x[lpart] - lower) / (center - lower)
     ret[rpart] = (upper - x[rpart]) / (upper - center)
     return ret
-
-
-def plot_linear_grid():
-    plt.close()
-    plt.figure(figsize=(5.5, 1.5))
-
-    cells = get_cells()
-    x = np.linspace(min(cells.keys()), max(cells.keys()), 100*100)
-    for k in cells.keys():
-        plt.axvline(x=k, color="DarkGray", linewidth=0.4, linestyle="--")
-
-    plt.plot(x, linear_element(cells, 3, x))
-    plt.plot(x, linear_element(cells, 4, x))
-    plt.plot(x, linear_element(cells, len(cells) - 1, x))
-    plt.xticks(list(cells.keys()), list(cells.values()))
 
 
 def quadratic_element(cells, n, k, x):
@@ -100,28 +117,34 @@ def quadratic_element(cells, n, k, x):
     return ret
 
 
+def plot_linear_grid():
+    plt.close()
+    fig = plt.figure(figsize=(5.5, 1.5))
+    ax = fig.add_subplot(111)
+
+    x, cells = get_cells()
+    plot_grid(cells)
+
+    plt.plot(x, linear_element(cells, 3, x))
+    plt.plot(x, linear_element(cells, 4, x))
+    plt.plot(x, linear_element(cells, len(cells) - 1, x))
+
+    plot_ticks(ax, cells)
+
+
 def plot_quadratic_grid():
     plt.close()
     fig = plt.figure(figsize=(5.5, 1.5))
     ax = fig.add_subplot(111)
 
-    cells = get_cells()
-    x = np.linspace(min(cells.keys()), max(cells.keys()), 100*100)
-    for k in cells.keys():
-        plt.axvline(x=k, color="DarkGray", linewidth=0.4, linestyle="--")
+    x, cells = get_cells()
+    plot_grid(cells)
 
     plt.plot(x, quadratic_element(cells, 3, 2, x))
     plt.plot(x, quadratic_element(cells, 4, 1, x))
     plt.plot(x, quadratic_element(cells, len(cells) - 1, 2, x))
 
-    keys = list(cells.keys())
-    keys_minor = []
-    for i, k in enumerate(keys):
-        if i > 0:
-            keys_minor.append(keys[i-1] + (keys[i] - keys[i-1]) / 2)
-    plt.xticks(keys, list(cells.values()))
-    ax.set_xticks(keys_minor, minor=True)
-    ax.set_xticklabels(len(keys_minor) * [""], minor=True)
+    plot_ticks(ax, cells, subticks=1)
 
 
 def main():
